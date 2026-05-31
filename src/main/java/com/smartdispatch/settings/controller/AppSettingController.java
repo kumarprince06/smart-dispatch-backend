64a955d17928e56518423672d784a1763f21e7b9
Kumar Prince <princekumarkolkata18@gmail.com>
2026-05-31 11:05:15 +0530

    refactor: update app settings endpoint mapping to /api/v1/app-settings

--- src/main/java/com/smartdispatch/settings/controller/AppSettingController.java
+++ src/main/java/com/smartdispatch/settings/controller/AppSettingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/settings")
+@RequestMapping("/api/v1/app-settings")
 @RequiredArgsConstructor
 public class AppSettingController {
 
